3081e12ae282b90f4de916dc8d4de6d68dad614a
m.matsushita <work.m.tanaka@gmail.com>
2020-08-01 22:14:24 +0900

    疑似要素からdivに変更

--- js/index.js
+++ js/index.js
@@ -47,3 +47,60 @@ var mySwiper = new Swiper('.swiper-container', {
 particlesJS.load('main', './lib/particles.json', function () {
     //console.log('callback - particles.js config loaded');
 });
+ /* =============== ローディングアニメーション =============== */
+ imagesProgress();
+
+ function imagesProgress () {
+
+     var $container    = $('#progress'),
+         $progressStart  = $container.find('.progress_start'),
+         $progressLoading  = $container.find('.progress_loading'),
+         $progressImg  = $container.find('.progress_img'),
+         imgLoad       = imagesLoaded('body'),
+         imgTotal      = imgLoad.images.length,
+         imgLoaded     = 0,
+         current       = 0,
+         progressTimer = setInterval(updateProgress, 1000 / 60);
+
+     imgLoad.on('progress', function () {
+         imgLoaded++;
+     });
+
+     function updateProgress () {
+         var target = (imgLoaded / imgTotal) * 100;
+         current += (target - current) * 0.1;
+
+         if(current >= 10){
+             $progressStart.animate({ left: '100%' }, 1000, 'easeInOutQuint');
+         }
+
+         if(current >= 100){
+             clearInterval(progressTimer);
+             $container.addClass('progress_complete');
+             $progressImg
+                 .animate({ opacity: 0 }, 250, function () {
+                     $container.animate({ left: '100%' }, 1000, 'easeInOutQuint');
+             });
+
+       setTimeout(function(){
+         $('#header').addClass('active');
+       },2000);
+
+       setTimeout(function(){
+         $('.top_content').addClass('open');
+       },1400);
+
+       setTimeout(function(){
+         $('.top_low_content').addClass('open');
+       },1800);
+
+       setTimeout(function(){
+         $('.contact_low_content').addClass('active');
+       },1800);
+         }
+
+         if (current > 99.9) {
+             current = 100;
+         }
+     }
+ }
