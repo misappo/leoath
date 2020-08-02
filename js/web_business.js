03326f5644e0d5fa3a0e00a913e011c38193530f
yuu-matsushita <h2.yuu.mat@gmail.com>
2020-08-02 23:21:55 +0900

    animetion修正

---
+++ js/web_business.js
@@ -0,0 +1,8 @@
+$('#w_production').on('inview', function (event, isInView) {
+    $(this).find('.img_wrapper').addClass('color');
+    $(this).find('.img_wrapper').css('opacity', 1);
+});
+$('#w_ad').on('inview', function (event, isInView) {
+    $(this).find('.img_wrapper').addClass('color');
+    $(this).find('.img_wrapper').css('opacity', 1);
+});
