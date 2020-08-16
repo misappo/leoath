d6b6a71b95c791aa8e920edc956c80e7992acfaf
m.matsushita <work.m.tanaka@gmail.com>
2020-08-16 18:06:50 +0900

    beauty追加と修正

--- js/beauty.js
+++ js/beauty.js
@@ -1,6 +1,6 @@
 var mySwiper = new Swiper('.swiper-container', {
     autoplay: {
-        delay: 3000,
+        delay: 3900,
     },
     effect: 'coverflow',
     slidesPerView: 'auto',
@@ -18,11 +18,52 @@ var mySwiper = new Swiper('.swiper-container', {
         type: 'bullets',
         paginationClickable: true,
     },
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
     loop: true,
+    speed: 420,
     centeredSlides: true,
-    //slideToClickedSlide: true,
+    slideToClickedSlide: true,
     spaceBetween: 230,
 },
     false);
+$(function ($) {
+    var content = $(".m_contents");
+    var sidebar = $(".m_heading");
+    var sticked = $(".m_heading_inner");
+
+    // サイドバーの位置
+    var sidebar_top = sidebar.offset().top;
+    // 固定するコンテンツの元々の位置
+    var sticked_original_top = sticked.offset().top;
+    // 固定するコンテンツの高さ
+    var sticked_height = sticked.height();
+
+    $(window).on('scroll resize', function () { // スクロールかリサイズ時
+        // 現在の位置
+        var scrollTop = $(document).scrollTop();
+        // メインコンテンツ最後尾
+        var content_bottom = content.offset().top + content.height();
+
+        if ((scrollTop > sticked_original_top - 76) && (scrollTop < content_bottom - sticked_height - 76)) {
+            // 現在位置が、初期位置より下で、メインコンテンツ最後尾より上なら、画面上部にサイドバーを固定
+            sticked.css({
+                'position': 'fixed',
+                'top': 76,
+                'width': sidebar.width()
+            });
+        } else if (scrollTop >= content_bottom - sticked_height -76) {
+            // 現在位置がメインコンテンツ最後尾より下なら、メインコンテンツ最後尾にサイドバーを位置させる
+            sticked.css({
+                'position': 'absolute',
+                'top': content_bottom - sticked_height - sidebar_top,
+                'width': sidebar.width()
+            });
+        } else {
+            // 現在位置が初期位置より上なら、何もしない
+            sticked.css({ 'position': 'static' });
+        }
+    });
+});
